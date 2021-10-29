be737c7435719fed827d42568f766131141cc66f
John Gruber <j.gruber@f5.com>
2021-10-29 12:08:53 -0500

    updated output for demo container resources

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,11 @@ output "k8s_api_ip" {
 output "kube_config" {
   value = "scp root@${ibm_is_floating_ip.floating_ip.address}:/root/.kube/config ${local.site_name}.config"
 }
+
+output "demo_container" {
+  value = "http://${ibm_is_floating_ip.floating_ip.address}:30080"
+}
+
+output "performance_target" {
+  value = "sockperf [test_type] -h ${ibm_is_floating_ip.floating_ip.address} -p 31111"
+}
